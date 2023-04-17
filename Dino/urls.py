7c4458e5b0caa0dde0dfda510d8fe933ade4e638
ibrahim-softsuave <ibrahim.softsuave@gmail.com>
2023-04-17 21:22:34 +0530

    front-end to back-end connection settings

--- Dino/urls.py
+++ Dino/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from .views import signinAPI,registerAPI
+# from .views import signinAPI,registerAPI
+from . import views
 
 urlpatterns=[
-    path('signin/',signinAPI.as_view()),
-    path('register/',registerAPI.as_view()),
-    # path('register/',views.register)
+    # path('signin/',signinAPI.as_view()),
+    # path('register/',registerAPI.as_view()),
+    path('register/',views.register),
+    path('signin/',views.signin),
+
 ]
--- Dino/views.py
+++ Dino/views.py
@@ -8,37 +8,8 @@ from django.views import View
 
 
 
-class signinAPI(View):
-    def post(self,request):
-        data=json.loads(request.body)
-        user=authenticate(request, username=data.get('username'), password=data.get('password'))
-        if user:
-            return HttpResponse ('Signin Successfully')
-        else:
-            username=CustomUser.objects.filter(username=data.get('username')).first()
-            if username is None:
-                 return HttpResponse ('Username is doesnot exist')
-            else:
-                 return HttpResponse ('Password is mismatched')
-class registerAPI(View):
-    def post(self,request):
-            data=json.loads(request.body)
-            if not data.get('username'):
-                 return HttpResponse('Username is missing')
-            else:
-                if data.get('password') != data.get('confirm_password'):
-                        return HttpResponse('password is mis matching')
-                else:
-                    user=CustomUser.objects.create_user(username=data.get('username'),email=data.get('email'),password=data.get('password'),phone_number=data.get('phone_number'))
-                    if user:
-                        return HttpResponse ('User Craete Successfully')
-                    else:
-                          return HttpResponse ('User Craete Failed')
-          
-
-# @csrf_exempt
-# def signin(request):
-#     if request.method == "POST":
+# class signinAPI(View):
+#     def post(self,request):
 #         data=json.loads(request.body)
 #         user=authenticate(request, username=data.get('username'), password=data.get('password'))
 #         if user:
@@ -49,9 +20,8 @@ class registerAPI(View):
 #                  return HttpResponse ('Username is doesnot exist')
 #             else:
 #                  return HttpResponse ('Password is mismatched')
-# @csrf_exempt
-# def register(request):
-#         if request.method == "POST":
+# class registerAPI(View):
+#     def post(self,request):
 #             data=json.loads(request.body)
 #             if not data.get('username'):
 #                  return HttpResponse('Username is missing')
@@ -64,4 +34,36 @@ class registerAPI(View):
 #                         return HttpResponse ('User Craete Successfully')
 #                     else:
 #                           return HttpResponse ('User Craete Failed')
-#         return HttpResponse('nousername')
+          
+
+@csrf_exempt
+def signin(request):
+    if request.method == "POST":
+        data=json.loads(request.body)
+        user=authenticate(request, username=data.get('username'), password=data.get('password'))
+        if user:
+            return HttpResponse ('Signin Successfully',200)
+        else:
+            username=CustomUser.objects.filter(username=data.get('username')).first()
+            if username is None:
+                 return HttpResponse ('Username is doesnot exist')
+            else:
+                 return HttpResponse ('Password is mismatched')
+    else:
+         return HttpResponse('other method')
+@csrf_exempt
+def register(request):
+        if request.method == "POST":
+            data=json.loads(request.body)
+            if not data.get('username'):
+                 return HttpResponse('Username is missing')
+            else:
+                if data.get('password') != data.get('confirm_password'):
+                        return HttpResponse('password is mis matching')
+                else:
+                    user=CustomUser.objects.create_user(username=data.get('username'),email=data.get('email'),password=data.get('password'),phone_number=data.get('phone_number'))
+                    if user:
+                        return HttpResponse ('User Craete Successfully')
+                    else:
+                          return HttpResponse ('User Craete Failed')
+        return HttpResponse('nousername')
